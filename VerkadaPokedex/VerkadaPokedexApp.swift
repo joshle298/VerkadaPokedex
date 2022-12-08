5842fcad2bcd0ff7b2f874f44b9392ff39625fe8
joshua-le <jle22@apple.com>
2022-12-07 20:02:49 -0500

    Initial Commit

---
+++ VerkadaPokedex/VerkadaPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerkadaPokedexApp.swift
+//  VerkadaPokedex
+//
+//  Created by Joshua Le on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct VerkadaPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
